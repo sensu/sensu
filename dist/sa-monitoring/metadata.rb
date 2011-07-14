e275f9bd8ed2291f33ee61612dfa1d56582832a2
Justin Kolberg <amd.prophet@gmail.com>
2011-07-14 15:44:48 -0700

    use template to generate config file for clients and servers

--- dist/sa-monitoring/metadata.rb
+++ dist/sa-monitoring/metadata.rb
@@ -1,4 +1,4 @@
-maintainer       "YOUR_COMPANY_NAME"
+maintainer       "Sonian, Inc."
 maintainer_email "YOUR_EMAIL"
 license          "All rights reserved"
 description      "Installs/Configures sa-monitoring"
--- dist/sa-monitoring/recipes/default.rb
+++ dist/sa-monitoring/recipes/default.rb
@@ -6,3 +6,13 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+databag = data_bag('sa-monitoring')
+
+template '/etc/sa-monitoring/config.json' do
+  mode 0600
+  variables(
+    'roles' => search(:role, '*:*'),
+    'checks' => databag['checks']
+  )
+end
