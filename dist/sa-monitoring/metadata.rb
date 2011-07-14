1147dc53c2c7f4b5f94651588cb29ce2919045df
Sean Porter <portertech@gmail.com>
2011-07-14 14:57:22 -0700

    distribution of checks is done w/ CM (OpsChef to start)

---
+++ dist/sa-monitoring/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "YOUR_COMPANY_NAME"
+maintainer_email "YOUR_EMAIL"
+license          "All rights reserved"
+description      "Installs/Configures sa-monitoring"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
---
+++ dist/sa-monitoring/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: sa-monitoring
+# Recipe:: default
+#
+# Copyright 2011, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
