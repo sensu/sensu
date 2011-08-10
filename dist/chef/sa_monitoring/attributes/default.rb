a3eec4aeeddc3d4c4a00908662be1f4117319118
Sean Porter <portertech@gmail.com>
2011-08-10 16:15:19 -0700

    sa-monitoring version bump and vhost path

--- dist/chef/sa_monitoring/attributes/default.rb
+++ dist/chef/sa_monitoring/attributes/default.rb
@@ -1,10 +1,10 @@
 # most of these values can be replaced with your method of discovery
-default.sa_monitoring.version = "0.0.5"
+default.sa_monitoring.version = "0.1.0"
 default.sa_monitoring.rabbitmq.host = "localhost"
 default.sa_monitoring.rabbitmq.port = 5671
 default.sa_monitoring.rabbitmq.ssl.cert_chain_file = "/etc/sa-monitoring/ssl/cert.pem"
 default.sa_monitoring.rabbitmq.ssl.private_key_file = "/etc/sa-monitoring/ssl/key.pem"
-default.sa_monitoring.rabbitmq.vhost = "sa-monitoring"
+default.sa_monitoring.rabbitmq.vhost = "/sa-monitoring"
 default.sa_monitoring.rabbitmq.user = "sa-monitoring"
 default.sa_monitoring.rabbitmq.password = "password"
 default.sa_monitoring.redis.host = "localhost"
