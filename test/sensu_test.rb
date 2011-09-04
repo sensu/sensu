ab2855583c61c8559830040755e22744b6325f3d
Sean Porter <portertech@gmail.com>
2011-09-04 11:32:32 -0700

    [tests] test_create_working_directory

--- test/sensu_test.rb
+++ test/sensu_test.rb
@@ -14,4 +14,10 @@ class TestSensu < MiniTest::Unit::TestCase
     settings = config.settings
     eventually(true) { settings.has_key?('client') }
   end
+
+  def test_create_working_directory
+    config = Sensu::Config.new(:config_file => File.join(File.dirname(__FILE__), 'config.json'))
+    config.create_working_directory
+    eventually(true) { File.exists?('/tmp/sensu') }
+  end
 end
