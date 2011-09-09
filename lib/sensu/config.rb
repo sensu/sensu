9e28912186e2c9a5958f46b8e22cb1e48e4fb29b
Sean Porter <portertech@gmail.com>
2011-09-09 09:15:48 -0700

    [validate-config] checks must meet certain conditions

--- lib/sensu/config.rb
+++ lib/sensu/config.rb
@@ -12,6 +12,21 @@ module Sensu
     def initialize(options={})
       config_file = options[:config_file] || '/etc/sensu/config.json'
       @settings = JSON.parse(File.open(config_file, 'r').read)
+      validate_config
+    end
+
+    def validate_config
+      @settings['checks'].each do |name, info|
+        unless info['interval'].is_a?(Integer) && info['interval'] > 0
+          raise 'configuration invalid, missing interval for check ' + name
+        end
+        unless info['command'].is_a?(String)
+          raise 'configuration invalid, missing command for check ' + name
+        end
+        unless info['subscribers'].is_a?(Array) && info['subscribers'].count > 0
+          raise 'configuration invalid, missing subscribers for check ' + name
+        end
+      end
     end
 
     def create_working_directory
