9540e662a3aea8f356d8f29a2314e2bf38abb38d
James Turnbull <james@lovedthanlost.net>
2011-09-18 08:30:21 +1000

    Initial cut of basic Puppet module
    It'll need some tweaking and refinement
    around setup and SSL.

---
+++ dist/puppet/sensu/manifests/client.pp
@@ -0,0 +1,26 @@
+class sensu::client {
+
+    require sensu
+    include sensu::params
+
+    $service = "client"
+    $sensu_user = $sensu::params::sensu_user
+
+    file { "/etc/sensu/plugins":
+      ensure => directory,
+      mode   => 0755,
+    }
+
+    file { "/etc/init/sensu-client.conf":
+      ensure  => file,
+      content => template("sensu/upstart.erb"),
+      mode    => 0644,
+    }
+
+    service { "sensu-client":
+      ensure    => running,
+      enable    => true,
+      subscribe => File["/etc/sensu/config.json"],
+      require   => File["/etc/init/sensu-server.conf"],
+    }
+}
