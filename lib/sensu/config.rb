51ce41ca8990147312bb94296c2213dc562c149e
Sean Porter <portertech@gmail.com>
2011-09-26 11:19:13 -0700

    [stdin] no longer creating event files, handlers read from STDIN, use blocking popen in defer for cross platform support :/

--- lib/sensu/config.rb
+++ lib/sensu/config.rb
@@ -58,19 +58,5 @@ module Sensu
       optparse.parse!(arguments)
       options
     end
-
-    def create_working_directory
-      begin
-        Dir.mkdir('/tmp/sensu')
-      rescue SystemCallError
-      end
-    end
-
-    def purge_working_directory
-      Dir.foreach('/tmp/sensu') do |file|
-        next if file == '.' || file == '..'
-        File.delete('/tmp/sensu/' + file)
-      end
-    end
   end
 end
