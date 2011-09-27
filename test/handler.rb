6d771fdbd646fbbf78b14e73af245cac1a39749d
Sean Porter <portertech@gmail.com>
2011-09-27 13:30:24 -0700

    [logging] http://docs.splunk.com/Documentation/Splunk/latest/Knowledge/UnderstandandusetheCommonInformationModel

--- test/handler.rb
+++ test/handler.rb
@@ -5,6 +5,9 @@ require 'json'
 
 event = JSON.parse(STDIN.read)
 
+puts 'name="Test Logging" event_id=handler:default reason="This should be first"'
+puts 'name="Test Logging" event_id=handler:default reason="This should be second"'
+
 File.open('/tmp/sensu_test_handlers', 'w') do |file|
   file.write(JSON.pretty_generate(event))
 end
