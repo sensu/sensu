134f38f6693b3a8ea612a5075791987b5546b614
James Turnbull <james@lovedthanlost.net>
2011-11-04 00:26:02 -0700

    Moved shebang

--- dist/puppet/sensu/files/plugins/puppet_agent.rb
+++ dist/puppet/sensu/files/plugins/puppet_agent.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/env ruby
 # Copyright 2011 James Turnbull
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,8 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-#!/usr/bin/env ruby
 #
 # Puppet Agent Plugin
 # ===
