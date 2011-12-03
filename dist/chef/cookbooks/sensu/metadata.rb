7f44be9a8cdc42fea08e383de2bfabed50e0db3d
Steve Lum <steve.lum@gmail.com>
2011-12-03 10:27:50 -0800

    updated version of sensu cookbook removed metadata.json

--- dist/chef/cookbooks/sensu/metadata.rb
+++ dist/chef/cookbooks/sensu/metadata.rb
@@ -3,13 +3,20 @@ maintainer_email "chefs@sonian.net"
 license          "Apache 2.0"
 description      "Installs/Configures Sensu"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.3"
+version          "0.0.4"
 
 # available @ http://community.opscode.com/cookbooks/rabbitmq
-depends "rabbitmq"
+depends "rabbitmq_sensu"
 
 # available @ http://community.opscode.com/cookbooks/redis-package
 depends "redis"
 
 # available @ http://community.opscode.com/cookbooks/apt
 depends "apt"
+
+#
+depends "yum"
+
+%w{ubuntu debian redhat centos}.each do |os|
+  supports os
+end
