3c15b81fc3d5b1b57840904de2a6b1c519111fa1
Sean Porter <portertech@gmail.com>
2011-12-15 11:33:27 -0800

    [routing] more verbose, but clearer

--- lib/sensu/client.rb
+++ lib/sensu/client.rb
@@ -126,8 +126,15 @@ module Sensu
         if check.key?('matching')
           @logger.info('[subscribe] -- check requires matching -- ' + check.name)
           matches = check.matching.all? do |key, value|
-            key = key.dup
-            (/^!/ === key ? !key.slice!(0) : true) == case key
+            desired = case key
+            when /^!/
+              key = key.dup
+              key.slice!(0)
+              false
+            else
+              true
+            end
+            matched = case key
             when 'subscribes'
               value.all? do |subscription|
                 @settings.client.subscriptions.include?(subscription)
@@ -135,6 +142,7 @@ module Sensu
             else
               @settings.client[key] == value
             end
+            desired == matched
           end
           if matches
             @logger.info('[subscribe] -- client matches -- ' + check.name)
