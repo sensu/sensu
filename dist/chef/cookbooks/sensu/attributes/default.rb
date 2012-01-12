b935575da86a033871cb2f4dc1eabbaddfaf6c94
Sean Porter <portertech@gmail.com>
2012-01-12 10:30:06 -0800

    version bump (beta)   - different metric check result behaviour, non-zero status triggers regular events   - client socket doesn't close, has simple response eg. ok, status defaults to 0   - chef cookbook updates, git/bundler deployed sandbox, optional firewall, new plugins

--- dist/chef/cookbooks/sensu/attributes/default.rb
+++ dist/chef/cookbooks/sensu/attributes/default.rb
@@ -1,5 +1,5 @@
 # most of these values can be replaced with your method of discovery
-default.sensu.version = "0.9.2"
+default.sensu.version = "0.9.3.beta"
 default.sensu.installation = "rubygems"
 default.sensu.directory = "/etc/sensu"
 default.sensu.log.directory = "/var/log/sensu"
