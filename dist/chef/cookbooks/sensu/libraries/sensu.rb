38f9e1aac136e7b29191c585e17e226a98e1b234
Sean Porter <portertech@gmail.com>
2012-01-12 09:18:34 -0800

    [metrics] mark metric event as flapping, minor style changes

--- dist/chef/cookbooks/sensu/libraries/sensu.rb
+++ dist/chef/cookbooks/sensu/libraries/sensu.rb
@@ -2,17 +2,17 @@ module Sensu
   def self.generate_config(node, databag)
     config = Hash.new
 
-    config.merge!(node.sensu.to_hash.reject {|key,value| %w[user version].include? key})
+    config.merge!(node.sensu.to_hash.reject { |key,value| %w[user version].include?(key) })
 
     address = (node.has_key? :ec2) ? node.ec2.public_ipv4 : node.ipaddress
 
-    config['client'].merge!({
+    config['client'].merge!(
       :name => node.name,
       :address => address,
       :subscriptions => node.roles
-    })
+    )
 
-    config.merge!(databag.reject {|key,value| %w[id chef_type data_bag].include? key})
+    config.merge!(databag.reject { |key,value| %w[id chef_type data_bag].include?(key) })
 
     JSON.pretty_generate(config)
   end
