45f372de7fed67dd14c665f584ca5b60266b8963
Sean Porter <portertech@gmail.com>
2012-01-11 21:28:47 -0800

    [chef] sensu-dashboard runs on a node w/ rubygems installation

--- dist/chef/cookbooks/sensu/recipes/dashboard.rb
+++ dist/chef/cookbooks/sensu/recipes/dashboard.rb
@@ -29,7 +29,7 @@ case node[:platform]
 when "ubuntu", "debian"
   template "/etc/init/sensu-dashboard.conf" do
     source "upstart.erb"
-    variables :bin_path => Sensu.find_bin_path, :service => "dashboard", :options => "-l #{node.sensu.log.directory}/sensu.log"
+    variables :service => "dashboard", :options => "-l #{node.sensu.log.directory}/sensu.log"
     mode 0644
   end
 
@@ -41,7 +41,7 @@ when "ubuntu", "debian"
 when "centos", "redhat"
   template "/etc/init.d/sensu-dashboard" do
     source "init.erb"
-    variables :bin_path => Sensu.find_bin_path, :service => "dashboard"
+    variables :service => "dashboard"
     mode 0755
   end
 
