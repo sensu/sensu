d21b5b4c380fc8c867bbe0f8ccafc244f308df2b
Sean Porter <portertech@gmail.com>
2012-04-15 01:25:14 -0700

    renamed a few testing files, added a before script for travis

--- test/handlers/stdout.rb
+++ test/handlers/stdout.rb
@@ -5,7 +5,7 @@ require 'json'
 
 event = JSON.parse(STDIN.read)
 
-puts 'stdout -- test logging -- this should be first'
-puts 'stdout -- test logging -- this should be second'
+puts 'stdout -- handler logging test -- this should be first'
+puts 'stdout -- handler logging test -- this should be second'
 
 puts 'stdout -- ' + event.to_json
