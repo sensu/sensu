123c4aca60a3f9e220114b5e2d441d0f56c68708
Sean Porter <portertech@gmail.com>
2012-04-25 18:27:00 -0700

    [refactor] begin work on the client

--- lib/sensu/client.rb
+++ lib/sensu/client.rb
@@ -36,19 +36,23 @@ module Sensu
     end
 
     def setup_rabbitmq
-      @logger.debug('[rabbitmq] -- connecting to rabbitmq')
-      @rabbitmq = AMQP.connect(@settings.rabbitmq.to_hash.symbolize_keys)
+      @logger.debug('connecting to rabbitmq', {
+        :settings => @settings.rabbitmq.to_hash
+      })
+      @rabbitmq = AMQP.connect(@settings.rabbitmq.to_hash)
       @amq = AMQP::Channel.new(@rabbitmq)
     end
 
     def publish_keepalive
-      @settings.client.timestamp = Time.now.to_i
-      @logger.debug('[keepalive] -- publishing keepalive -- ' + @settings.client.timestamp.to_s)
-      @amq.queue('keepalives').publish(@settings.client.to_json)
+      payload = @settings.client.to_hash.merge(:timestamp => Time.now.to_i)
+      @logger.debug('publishing keepalive', {
+        :payload => payload
+      })
+      @amq.queue('keepalives').publish(payload.to_json)
     end
 
     def setup_keepalives
-      @logger.debug('[keepalive] -- setup keepalives')
+      @logger.debug('scheduling keepalives')
       publish_keepalive
       @timers << EM::PeriodicTimer.new(30) do
         unless @rabbitmq.reconnecting?
@@ -58,21 +62,28 @@ module Sensu
     end
 
     def publish_result(check)
-      @logger.info('[result] -- publishing check result -- ' + [check.name, check.status, check.output.gsub(/\n/, '\n')].join(' -- '))
-      @amq.queue('results').publish({
+      payload = {
         :client => @settings.client.name,
-        :check => check.to_hash
-      }.to_json)
+        :check => check
+      }
+      @logger.info('publishing check result', {
+        :payload => payload
+      })
+      @amq.queue('results').publish(payload.to_json)
     end
 
     def execute_check(check)
-      @logger.debug('[execute] -- attempting to execute check -- ' + check.name)
-      if @settings.checks.key?(check.name)
-        unless @checks_in_progress.include?(check.name)
-          @logger.debug('[execute] -- executing check -- ' + check.name)
-          @checks_in_progress.push(check.name)
+      @logger.debug('attempting to execute check', {
+        :check => check
+      })
+      if @settings.checks.key?(check[:name])
+        unless @checks_in_progress.include?(check[:name])
+          @logger.debug('executing check', {
+            :check => check
+          })
+          @checks_in_progress.push(check[:name])
           unmatched_tokens = Array.new
-          command = @settings.checks[check.name].command.gsub(/:::(.*?):::/) do
+          command = @settings.checks[check[:name]].command.gsub(/:::(.*?):::/) do
             token = $1.to_s
             begin
               value = @settings.client.instance_eval(token)
@@ -90,77 +101,98 @@ module Sensu
               started = Time.now.to_f
               begin
                 IO.popen(command + ' 2>&1') do |io|
-                  check.output = io.read
+                  check[:output] = io.read
                 end
-                check.status = $?.exitstatus
+                check[:status] = $?.exitstatus
               rescue => error
-                @logger.warn('[execute] -- unexpected error: ' + error.to_s)
-                check.output = 'Unexpected error: ' + error.to_s
-                check.status = 2
+                @logger.warn('unexpected error', {
+                  :error => error.to_s
+                })
+                check[:output] = 'Unexpected error: ' + error.to_s
+                check[:status] = 2
               end
-              check.duration = ('%.3f' % (Time.now.to_f - started)).to_f
+              check[:duration] = ('%.3f' % (Time.now.to_f - started)).to_f
             end
             publish = proc do
-              unless check.status.nil?
+              unless check[:status].nil?
                 publish_result(check)
               else
-                @logger.warn('[execute] -- nil exit status code -- ' + check.name)
+                @logger.warn('nil exit status code', {
+                  :check => check
+                })
               end
-              @checks_in_progress.delete(check.name)
+              @checks_in_progress.delete(check[:name])
             end
             EM::defer(execute, publish)
           else
-            @logger.warn('[execute] -- missing client attributes -- ' + unmatched_tokens.join(', ') + ' -- ' + check.name)
-            check.output = 'Missing client attributes: ' + unmatched_tokens.join(', ')
-            check.status = 3
-            check.handle = false
+            @logger.warn('missing client attributes', {
+              :check => check,
+              :unmatched_tokens => unmatched_tokens
+            })
+            check[:output] = 'Missing client attributes: ' + unmatched_tokens.join(', ')
+            check[:status] = 3
+            check[:handle] = false
             publish_result(check)
-            @checks_in_progress.delete(check.name)
+            @checks_in_progress.delete(check[:name])
           end
         else
-          @logger.warn('[execute] -- previous check execution still in progress -- ' + check.name)
+          @logger.warn('previous check execution in progress', {
+            :check => check
+          })
         end
       else
-        @logger.warn('[execute] -- unknown check -- ' + check.name)
-        check.output = 'Unknown check'
-        check.status = 3
-        check.handle = false
+        @logger.warn('unknown check', {
+          :check => check
+        })
+        check[:output] = 'Unknown check'
+        check[:status] = 3
+        check[:handle] = false
         publish_result(check)
-        @checks_in_progress.delete(check.name)
+        @checks_in_progress.delete(check[:name])
       end
     end
 
     def setup_subscriptions
-      @logger.debug('[subscribe] -- setup subscriptions')
+      @logger.debug('subscribing to client subscriptions')
       @uniq_queue_name ||= rand(36**32).to_s(36)
       @check_request_queue = @amq.queue(@uniq_queue_name, :auto_delete => true)
       @settings.client.subscriptions.uniq.each do |exchange|
-        @logger.debug('[subscribe] -- queue binding to exchange -- ' + exchange)
+        @logger.debug('binding queue to exchange', {
+          :queue => @check_request_queue,
+          :exchange => exchange
+        })
         @check_request_queue.bind(@amq.fanout(exchange))
       end
-      @check_request_queue.subscribe do |check_request_json|
+      @check_request_queue.subscribe do |payload|
         begin
-          check = Hashie::Mash.new(JSON.parse(check_request_json))
-          if check.name.is_a?(String) && check.issued.is_a?(Integer)
-            @logger.info('[subscribe] -- received check request -- ' + check.name)
+          check = JSON.parse(payload, :symbolize_names => true)
+          if check[:name].is_a?(String) && check[:issued].is_a?(Integer)
+            @logger.info('received check request', {
+              :check => check
+            })
             execute_check(check)
           else
-            @logger.warn('[subscribe] -- invalid check request: ' + check_request_json)
+            @logger.warn('invalid check request', {
+              :check => check
+            })
           end
         rescue JSON::ParserError => error
-          @logger.warn('[subscribe] -- check request must be valid json: ' + error.to_s)
+          @logger.warn('check request payload must be valid json', {
+            :payload => payload,
+            :error => error.to_s
+          })
         end
       end
     end
 
     def setup_rabbitmq_monitor
-      @logger.debug('[monitor] -- setup rabbitmq monitor')
+      @logger.debug('monitoring rabbitmq connection')
       @timers << EM::PeriodicTimer.new(5) do
         if @rabbitmq.reconnecting?
-          @logger.warn('[monitor] -- reconnecting to rabbitmq')
+          @logger.warn('reconnecting to rabbitmq')
         else
           unless @check_request_queue.subscribed?
-            @logger.warn('[monitor] -- re-subscribing to client subscriptions')
+            @logger.warn('re-subscribing to client subscriptions')
             setup_subscriptions
           end
         end
@@ -168,18 +200,18 @@ module Sensu
     end
 
     def setup_standalone(options={})
-      @logger.debug('[standalone] -- setup standalone checks')
+      @logger.debug('scheduling standalone checks')
       standalone_check_count = 0
       @settings.checks.each do |name, details|
         if details.standalone
           standalone_check_count += 1
-          check = Hashie::Mash.new(details.merge(:name => name))
+          check = details.to_hash.merge(:name => name)
           stagger = options[:test] ? 0 : 7
           @timers << EM::Timer.new(stagger*standalone_check_count) do
             interval = options[:test] ? 0.5 : details.interval
             @timers << EM::PeriodicTimer.new(interval) do
               unless @rabbitmq.reconnecting?
-                check.issued = Time.now.to_i
+                check[:issued] = Time.now.to_i
                 execute_check(check)
               end
             end
@@ -189,8 +221,8 @@ module Sensu
     end
 
     def setup_socket
-      @logger.debug('[socket] -- starting up client tcp socket')
-      EM::start_server('127.0.0.1', 3030, ClientSocket) do |socket|
+      @logger.debug('binding client tcp socket')
+      EM::start_server('127.0.0.1', 3030, ::Sensu::Socket) do |socket|
         socket.settings = @settings
         socket.logger = @logger
         socket.amq = @amq
@@ -198,14 +230,16 @@ module Sensu
     end
 
     def stop_reactor
-      @logger.info('[stop] -- completing checks in progress')
+      @logger.info('completing checks in progress', {
+        :checks_in_progress => @checks_in_progress
+      })
       complete_in_progress = EM::tick_loop do
         if @checks_in_progress.empty?
           :stop
         end
       end
       complete_in_progress.on_stop do
-        @logger.warn('[stop] -- stopping reactor')
+        @logger.warn('stopping reactor')
         EM::PeriodicTimer.new(0.25) do
           EM::stop_event_loop
         end
@@ -228,35 +262,45 @@ module Sensu
     end
   end
 
-  class ClientSocket < EM::Connection
+  class Socket < EM::Connection
     attr_accessor :settings, :logger, :amq
 
     def receive_data(data)
       if data == 'ping'
-        @logger.debug('[socket] -- received ping')
+        @logger.debug('socket received ping')
         send_data('pong')
       else
-        @logger.debug('[socket] -- received data -- ' + data)
+        @logger.debug('socket received data', {
+          :data => data
+        })
         begin
-          check = Hashie::Mash.new(JSON.parse(data))
-          validates = %w[name output].all? do |key|
+          check = JSON.parse(data, :symbolize_names => true)
+          validates = [:name, :output].all? do |key|
             check[key].is_a?(String)
           end
-          check.issued = Time.now.to_i
-          check.status ||= 0
-          if validates && check.status.is_a?(Integer)
-            @logger.info('[socket] -- publishing check result -- ' + [check.name, check.status, check.output.gsub(/\n/, '\n')].join(' -- '))
-            @amq.queue('results').publish({
+          check[:issued] ||= Time.now.to_i
+          check[:status] ||= 0
+          if validates && check[:status].is_a?(Integer)
+            payload = {
               :client => @settings.client.name,
-              :check => check.to_hash
-            }.to_json)
+              :check => check
+            }
+            @logger.info('publishing check result', {
+              :payload => payload
+            })
+            @amq.queue('results').publish(payload.to_json)
             send_data('ok')
           else
-            @logger.warn('[socket] -- check name and output must be strings, status defaults to 0 -- e.g. {"name": "x", "output": "y"}')
+            @logger.warn('invalid check result', {
+              :check => check
+            })
             send_data('invalid')
           end
         rescue JSON::ParserError => error
-          @logger.warn('[socket] -- check result must be valid json: ' + error.to_s)
+          @logger.warn('check result must be valid json', {
+            :data => data,
+            :error => error.to_s
+          })
           send_data('invalid')
         end
       end
