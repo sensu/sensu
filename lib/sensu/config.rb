df4212c3665d9008b1125de46c933b0d534771cd
Sean Porter <portertech@gmail.com>
2012-05-01 11:28:25 -0700

    [cli] break cli arg parsing out of config, gets its own class

--- lib/sensu/config.rb
+++ lib/sensu/config.rb
@@ -10,7 +10,9 @@ require 'cabin'
 require 'amqp'
 
 require File.join(File.dirname(__FILE__), 'constants')
+require File.join(File.dirname(__FILE__), 'cli')
 require File.join(File.dirname(__FILE__), 'logger')
+
 require File.join(File.dirname(__FILE__), 'patches', 'ruby')
 require File.join(File.dirname(__FILE__), 'patches', 'amqp')
 
@@ -199,40 +201,6 @@ module Sensu
         end
       end
       validate_settings
-    end
-
-    def self.read_arguments(arguments)
-      options = Hash.new
-      optparse = OptionParser.new do |opts|
-        opts.on('-h', '--help', 'Display this message') do
-          puts opts
-          exit
-        end
-        opts.on('-V', '--version', 'Display version') do
-          puts Sensu::VERSION
-          exit
-        end
-        opts.on('-c', '--config FILE', 'Sensu JSON config FILE. Default is /etc/sensu/config.json') do |file|
-          options[:config_file] = file
-        end
-        opts.on('-d', '--config_dir DIR', 'DIR for supplemental Sensu JSON config files. Default is /etc/sensu/conf.d/') do |dir|
-          options[:config_dir] = dir
-        end
-        opts.on('-l', '--log FILE', 'Log to a given FILE. Default is to log to stdout') do |file|
-          options[:log_file] = file
-        end
-        opts.on('-v', '--verbose', 'Enable verbose logging') do
-          options[:verbose] = true
-        end
-        opts.on('-b', '--background', 'Fork into the background') do
-          options[:daemonize] = true
-        end
-        opts.on('-p', '--pid_file FILE', 'Write the PID to a given FILE') do |file|
-          options[:pid_file] = file
-        end
-      end
-      optparse.parse!(arguments)
-      Sensu::DEFAULT_OPTIONS.merge(options)
     end
   end
 end
