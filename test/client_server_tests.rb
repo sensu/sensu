70fbefcd674b41c328d6ab72523324c92221be96
Sean Porter <portertech@gmail.com>
2012-07-19 11:34:09 -0700

    [socket-handlers] test tcp then udp

--- test/client_server_tests.rb
+++ test/client_server_tests.rb
@@ -142,6 +142,10 @@ class TestSensuClientServer < TestCase
     server.setup_results
     client.setup_sockets
     EM::Timer.new(1) do
+      EM::connect('127.0.0.1', 3030, nil) do |socket|
+        socket.send_data('{"name": "tcp_socket", "output": "tcp", "status": 1}')
+        socket.close_connection_after_writing
+      end
       EM::open_datagram_socket('127.0.0.1', 0, nil) do |socket|
         data = '{"name": "udp_socket", "output": "udp", "status": 1}'
         2.times do
@@ -149,15 +153,11 @@ class TestSensuClientServer < TestCase
         end
         socket.close_connection_after_writing
       end
-      EM::connect('127.0.0.1', 3030, nil) do |socket|
-        socket.send_data('{"name": "tcp_socket", "output": "tcp", "status": 1}')
-        socket.close_connection_after_writing
-      end
     end
     EM::Timer.new(3) do
       server.redis.hgetall('events:' + @settings[:client][:name]).callback do |events|
-        assert(events.include?('udp_socket'))
         assert(events.include?('tcp_socket'))
+        assert(events.include?('udp_socket'))
         done
       end
     end
