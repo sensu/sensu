4381070ccbd2cdb479060f5413546a4a46b39031
Sean Porter <portertech@gmail.com>
2012-12-06 03:32:26 +0000

    [filters] added test for negate: true

--- test/filter_event_data_tests.rb
+++ test/filter_event_data_tests.rb
@@ -3,9 +3,11 @@ class TestSensuFilterEventData < TestCase
     server = Sensu::Server.new(@options)
     event = event_template
     assert(!server.event_filtered?('action', event))
-    assert(!server.event_filtered?('status', event))
+    assert(server.event_filtered?('action_negated', event))
     event[:action] = 'resolve'
     assert(server.event_filtered?('action', event))
+    assert(!server.event_filtered?('action_negated', event))
+    assert(!server.event_filtered?('status', event))
     event[:check][:status] = 2
     assert(server.event_filtered?('status', event))
     done
