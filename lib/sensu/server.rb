51fb653d1313d3ddb542c7c0b7f794fe84343344
Sean Porter <portertech@gmail.com>
2012-12-06 02:36:56 +0000

    [filters] hash_values_equal? is a private method

--- lib/sensu/server.rb
+++ lib/sensu/server.rb
@@ -162,24 +162,10 @@ module Sensu
       end
     end
 
-    def equal_hash_values?(hash_one, hash_two)
-      hash_one.keys.all? do |key|
-        if hash_one[key] == hash_two[key]
-          true
-        else
-          if hash_one[key].is_a?(Hash) && hash_two[key].is_a?(Hash)
-            equal_hash_values?(hash_one[key], hash_two[key])
-          else
-            false
-          end
-        end
-      end
-    end
-
     def event_filtered?(filter_name, event)
       if @settings.filter_exists?(filter_name)
         filter = @settings[:filters][filter_name]
-        matched = equal_hash_values?(filter[:attributes], event)
+        matched = hash_values_equal?(filter[:attributes], event)
         filter[:negate] ? matched : !matched
       else
         @logger.error('unknown filter', {
@@ -759,6 +745,20 @@ module Sensu
       EM::Timer.new(wait) do
         unless block.call
           retry_until_true(wait, &block)
+        end
+      end
+    end
+
+    def hash_values_equal?(hash_one, hash_two)
+      hash_one.keys.all? do |key|
+        if hash_one[key] == hash_two[key]
+          true
+        else
+          if hash_one[key].is_a?(Hash) && hash_two[key].is_a?(Hash)
+            hash_values_equal?(hash_one[key], hash_two[key])
+          else
+            false
+          end
         end
       end
     end
