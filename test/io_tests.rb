0b5c4baeee7416d1594367070643ca393b8ad0c2
Sean Porter <portertech@gmail.com>
2012-12-30 20:49:44 +0000

    [popen] ruby version sensitive tests

--- test/io_tests.rb
+++ test/io_tests.rb
@@ -1,15 +1,20 @@
 class TestSensuIO < TestCase
   def test_popen_successful_command
     output, status = Sensu::IO.popen('echo -n "test"')
-    assert_equal('test', output)    
+    assert_equal('test', output)
     assert_equal(0, status)
     done
   end
 
   def test_popen_timed_out_command
-    output, status = Sensu::IO.popen('sleep 10', 'r', 0.25)
-    assert_equal('Execution timed out', output)    
-    assert_equal(2, status)
+    output, status = Sensu::IO.popen('sleep 2 && echo "Ruby 1.8"', 'r', 0.25)
+    if RUBY_VERSION < '1.9.0'
+      assert_equal('Ruby 1.8', output)
+      assert_equal(0, status)
+    else
+      assert_equal('Execution timed out', output)
+      assert_equal(2, status)
+    end
     done
   end
 
