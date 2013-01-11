a0abc3de28795f8f8a9d6a38b9b6cca00dbd886c
Sean Porter <portertech@gmail.com>
2013-01-11 16:16:43 +0000

    [rspec] cli spec

---
+++ spec/cli_spec.rb
@@ -0,0 +1,30 @@
+require File.dirname(__FILE__) + '/../lib/sensu/cli.rb'
+require File.dirname(__FILE__) + '/../lib/sensu/constants.rb'
+
+describe 'Sensu::CLI' do
+  it 'can provide default configuration options' do
+    Sensu::CLI.read.sort.should eq(Sensu::DEFAULT_OPTIONS.sort)
+  end
+
+  it 'can parse command line arguments' do
+    options = Sensu::CLI.read([
+      '-c', 'spec/config.json',
+      '-d', 'spec/conf.d',
+      '-e', 'spec/extensions',
+      '-v',
+      '-l', '/tmp/sensu_spec.log',
+      '-p', '/tmp/sensu_spec.pid',
+      '-b'
+    ])
+    expected = {
+      :config_file => 'spec/config.json',
+      :config_dir => 'spec/conf.d',
+      :extension_dir => 'spec/extensions',
+      :log_level => :debug,
+      :log_file => '/tmp/sensu_spec.log',
+      :pid_file => '/tmp/sensu_spec.pid',
+      :daemonize => true
+    }
+    options.sort.should eq(expected.sort)
+  end
+end
