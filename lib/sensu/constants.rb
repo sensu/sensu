1ea5193c2da65eb5627d7ba63cf5634aac5f6cc5
Sean Porter <portertech@gmail.com>
2013-01-12 07:29:59 +0000

    [version] beta version bump, updated changelog

--- lib/sensu/constants.rb
+++ lib/sensu/constants.rb
@@ -1,6 +1,6 @@
 module Sensu
   unless defined?(Sensu::VERSION)
-    VERSION = '0.9.9.beta.2'
+    VERSION = '0.9.9.beta.3'
 
     DEFAULT_OPTIONS = {
       :config_file => '/etc/sensu/config.json',
