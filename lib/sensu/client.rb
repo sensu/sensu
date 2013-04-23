98199aacb4c2669c380c8ed43f0650353a49b932
Piavlo <lolitushka@gmail.com>
2013-04-23 18:26:59 +0300

    send back event with substituted command tokens

--- lib/sensu/client.rb
+++ lib/sensu/client.rb
@@ -99,6 +99,7 @@ module Sensu
         command, unmatched_tokens = substitute_command_tokens(check)
         check[:executed] = Time.now.to_i
         if unmatched_tokens.empty?
+          check[:command] = command
           execute = Proc.new do
             @logger.debug('executing check command', {
               :check => check
