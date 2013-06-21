382cf80360960856002cbe8eba942df79475d542
Piavlo <lolitushka@gmail.com>
2013-06-21 10:58:54 +0300

    one more

--- lib/sensu/utilities.rb
+++ lib/sensu/utilities.rb
@@ -63,7 +63,7 @@ module Sensu
         if secrets.include?(key.to_s)
           hash[key] = "ANONYMIZED"
         elsif value.is_a?(Hash)
-          hash[key] = redact_passwords(value)
+          hash[key] = anonymize(value)
         end
       end
       hash
