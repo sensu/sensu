cd40065605213c28fcd006674151e8aa6ac22bcb
Sean Porter <portertech@gmail.com>
2013-07-16 21:07:31 +0000

    exited?() can let nil exit statuses through

--- lib/sensu/io.rb
+++ lib/sensu/io.rb
@@ -68,7 +68,7 @@ module Sensu
         if wait_on_group
           wait_on_process_group(process.pid)
         end
-        [output, status.exited? ? status.exitstatus : 2]
+        [output, status.exitstatus || 2]
       end
     end
   end
