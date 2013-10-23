e142bc3ae2f87131eda9cea56a36f5f9d8825bb9
Sean Porter <portertech@gmail.com>
2013-10-22 23:05:05 -0700

    [profilers] wrote a gc profiler

--- spec/profilers/ruby_profiler.rb
+++ spec/profilers/ruby_profiler.rb
@@ -12,15 +12,18 @@ module Sensu
       end
 
       def post_init
-        @report_path = '/tmp/ruby_profiler.html'
         RubyProf.start
+      end
+
+      def report_path
+        '/tmp/ruby_profiler.' + ::Process.pid.to_s + '.html'
       end
 
       def stop
         print = Proc.new do
-          report = RubyProf.stop
-          printer = RubyProf::GraphHtmlPrinter.new(report)
-          File.open(@report_path, 'w') do |file|
+          result = RubyProf.stop
+          printer = RubyProf::GraphHtmlPrinter.new(result)
+          File.open(report_path, 'w') do |file|
             printer.print(file, {
               :print_file => true,
               :min_percent => 10
@@ -28,8 +31,8 @@ module Sensu
           end
         end
         complete = Proc.new do
-          @logger.info('generated html profile report', {
-            :path => @report_path
+          logger.info('ruby profiler generated an html report', {
+            :path => report_path
           })
           yield
         end
