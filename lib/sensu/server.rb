d25d8a6298c623088a023251e84554b4ee1be8c7
rwxybh <rwxybh@126.com>
2013-11-06 14:49:25 +0800

    Add ignore option for changing keep-alive check dynamically!

--- lib/sensu/server.rb
+++ lib/sensu/server.rb
@@ -573,7 +573,8 @@ module Sensu
                 :thresholds => {
                   :warning => 120,
                   :critical => 180
-                }
+                },
+                :ignore => false
               }
               if client.has_key?(:keepalive)
                 check = deep_merge(check, client[:keepalive])
@@ -583,6 +584,9 @@ module Sensu
               check[:executed] = Time.now.to_i
               time_since_last_keepalive = Time.now.to_i - client[:timestamp]
               case
+              when check[:ignore]
+                check[:output] = 'Ignore keep-alive check for this client'
+                check[:status] = 0
               when time_since_last_keepalive >= check[:thresholds][:critical]
                 check[:output] = 'No keep-alive sent from client in over '
                 check[:output] << check[:thresholds][:critical].to_s + ' seconds'
