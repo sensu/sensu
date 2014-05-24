baa8fdb348bcfc6f9b3d8990d1c752485e6617dd
Sean Porter <portertech@gmail.com>
2014-05-24 00:26:41 -0700

    [libraries] test all utility methods

--- spec/utilities_spec.rb
+++ spec/utilities_spec.rb
@@ -1,7 +1,66 @@
-require File.dirname(__FILE__) + '/../lib/sensu/utilities.rb'
+require File.dirname(__FILE__) + '/helpers.rb'
+require 'sensu/utilities'
 
 describe 'Sensu::Utilities' do
+  include Helpers
   include Sensu::Utilities
+
+  it 'can determine that we are testing' do
+    expect(testing?).to be_true
+  end
+
+  it 'can retry a block call until it returns true' do
+    async_wrapper do
+      times = 0
+      retry_until_true(0.05) do
+        times +=1
+        times == 3
+      end
+      timer(0.5) do
+        expect(times).to eq(3)
+        async_done
+      end
+    end
+  end
+
+  it 'can deep merge two hashes' do
+    hash_one = {
+      :foo => 1,
+      :bar => {
+        :one => 1,
+        :two => {
+          :three => 3
+        }
+      },
+      :baz => ["one"],
+      :qux => 42
+    }
+    hash_two = {
+      :foo => 42,
+      :bar => {
+        :one => "one",
+        :two => {
+          :three => 3,
+          :four => 4
+        }
+      },
+      :baz => ["one", "two", "three"],
+      :qux => [42]
+    }
+    expected = {
+      :foo => 42,
+      :bar => {
+        :one => "one",
+        :two => {
+          :three => 3,
+          :four => 4
+        }
+      },
+      :baz => ["one", "two", "three"],
+      :qux => [42]
+    }
+    expect(deep_merge(hash_one, hash_two)).to eq(expected)
+  end
 
   it 'can redact sensitive info from a hash' do
     hash = {
