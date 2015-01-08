65852e138e94ef7263d5aa511712e59bc9b0eb4c
Sean Porter <portertech@gmail.com>
2015-01-07 18:46:55 -0800

    [refactoring] documented redis

--- lib/sensu/redis.rb
+++ lib/sensu/redis.rb
@@ -4,8 +4,13 @@ require "em-redis"
 
 module Sensu
   class Redis
+    # Connect to Redis and ensure that the Redis version is at least
+    # 1.3.14, in order to support certain commands.
+    #
+    # @param options [Hash]
+    # @return [Object] Redis connection object.
     def self.connect(options={})
-      options ||= Hash.new
+      options ||= {}
       connection = EM::Protocols::Redis.connect(options)
       connection.info do |info|
         if info[:redis_version] < "1.3.14"
