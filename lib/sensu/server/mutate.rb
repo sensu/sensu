834dbcf783c1a658c4f55cb4a058f6608ecd1bba
Sean Porter <portertech@gmail.com>
2015-02-11 14:05:26 -0800

    [refactoring] documented server mutate

--- lib/sensu/server/mutate.rb
+++ lib/sensu/server/mutate.rb
@@ -1,6 +1,17 @@
 module Sensu
   module Server
     module Mutate
+      # Create a mutator callback (Proc). A mutator callback takes two
+      # parameters, for the mutator output and status code. The
+      # created callback can be used for standard mutators and mutator
+      # extensions. The provided callback will only be called when the
+      # mutator status is `0` (OK). If the status is not `0`, an error
+      # is logged, and the `@handling_event_count` is decremented by
+      # `1`.
+      #
+      # @param mutator [Object] definition or extension.
+      # @param event [Hash] data.
+      # @param callback [Proc] to call when the mutator status is `0`.
       def mutator_callback(mutator, event, &callback)
         Proc.new do |output, status|
           if status == 0
@@ -18,17 +29,47 @@ module Sensu
         end
       end
 
+      # Execute a standard mutator (pipe), spawn a process using the
+      # mutator command and pipe the event data to it via STDIN. The
+      # `mutator_callback()` method is used to create the mutator
+      # callback, wrapping the provided callback (event handler).
+      #
+      # @param mutator [Hash] definition.
+      # @param event [Hash] data.
+      # @param callback [Proc] to call when the mutator executes
+      #   successfully.
       def pipe_mutator(mutator, event, &callback)
         options = {:data => MultiJson.dump(event), :timeout => mutator[:timeout]}
         block = mutator_callback(mutator, event, &callback)
         Spawn.process(mutator[:command], options, &block)
       end
 
+      # Run a mutator extension, within the Sensu EventMachine reactor
+      # (event loop). The `mutator_callback()` method is used to
+      # create the mutator callback, wrapping the provided callback
+      # (event handler).
+      #
+      # @param mutator [Object] extension.
+      # @param event [Hash] data.
+      # @param callback [Proc] to call when the mutator runs
+      #   successfully.
       def mutator_extension(mutator, event, &callback)
         block = mutator_callback(mutator, event, &callback)
         mutator.safe_run(event, &block)
       end
 
+      # Mutate event data for a handler. By default, the "json"
+      # mutator is used, unless the handler specifies another mutator.
+      # If a mutator does not exist, not defined or a missing
+      # extension, an error will be logged and the
+      # `@handling_event_count` is decremented by `1`. This method
+      # first checks for the existence of a standard mutator, then
+      # checks for an extension if a standard mutator is not defined.
+      #
+      # @param handler [Hash] definition.
+      # @param event [Hash] data.
+      # @param callback [Proc] to call when the mutator executes/runs
+      #   successfully (event handler).
       def mutate_event(handler, event, &callback)
         mutator_name = handler[:mutator] || "json"
         case
