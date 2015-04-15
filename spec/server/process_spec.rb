51302e698e5b823eb65add9fd48f63a713907855
Sean Porter <portertech@gmail.com>
2015-04-14 23:15:26 -0700

    [clients] test result storage

--- spec/server/process_spec.rb
+++ spec/server/process_spec.rb
@@ -146,15 +146,22 @@ describe "Sensu::Server::Process" do
             transport.publish(:direct, "results", MultiJson.dump(result))
             transport.publish(:direct, "results", MultiJson.dump(result))
             timer(3) do
-              redis.hget("events:i-424242", "test") do |event_json|
-                event = MultiJson.load(event_json)
-                expect(event[:id]).to be_kind_of(String)
-                expect(event[:check][:status]).to eq(1)
-                expect(event[:occurrences]).to eq(2)
-                timer(2) do
-                  latest_event_file = IO.read("/tmp/sensu-event.json")
-                  expect(MultiJson.load(latest_event_file)).to eq(event)
-                  async_done
+              redis.sismember("result:i-424242", "test") do |is_member|
+                expect(is_member).to be(true)
+                redis.get("result:i-424242:test") do |result_json|
+                  result = MultiJson.load(result_json)
+                  expect(result[:output]).to eq("WARNING")
+                  redis.hget("events:i-424242", "test") do |event_json|
+                    event = MultiJson.load(event_json)
+                    expect(event[:id]).to be_kind_of(String)
+                    expect(event[:check][:status]).to eq(1)
+                    expect(event[:occurrences]).to eq(2)
+                    timer(2) do
+                      latest_event_file = IO.read("/tmp/sensu-event.json")
+                      expect(MultiJson.load(latest_event_file)).to eq(event)
+                      async_done
+                    end
+                  end
                 end
               end
             end
