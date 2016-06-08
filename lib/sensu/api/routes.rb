28c5bf960671817c403d76677338a065afc39082
Sean Porter <portertech@gmail.com>
2016-06-08 15:26:20 -0700

    [api-rewrite] working event routes

--- lib/sensu/api/routes.rb
+++ lib/sensu/api/routes.rb
@@ -3,6 +3,7 @@ require "sensu/api/routes/health"
 require "sensu/api/routes/clients"
 require "sensu/api/routes/checks"
 require "sensu/api/routes/request"
+require "sensu/api/routes/events"
 
 module Sensu
   module API
@@ -12,6 +13,7 @@ module Sensu
       include Clients
       include Checks
       include Request
+      include Events
     end
   end
 end
---
+++ lib/sensu/api/routes/events.rb
@@ -0,0 +1,76 @@
+require "sensu/api/utilities/resolve_event"
+
+module Sensu
+  module API
+    module Routes
+      module Events
+        include Utilities::ResolveEvent
+
+        EVENTS_URI = "/events".freeze
+        EVENTS_CLIENT_URI = /^\/events\/([\w\.-]+)$/
+        EVENT_URI = /^\/events\/([\w\.-]+)\/([\w\.-]+)$/
+
+        def get_events
+          @response_content = []
+          @redis.smembers("clients") do |clients|
+            unless clients.empty?
+              clients.each_with_index do |client_name, index|
+                @redis.hgetall("events:#{client_name}") do |events|
+                  events.each do |check_name, event_json|
+                    @response_content << Sensu::JSON.load(event_json)
+                  end
+                  if index == clients.length - 1
+                    respond
+                  end
+                end
+              end
+            else
+              respond
+            end
+          end
+        end
+
+        def get_events_client
+          client_name = EVENTS_CLIENT_URI.match(@http_request_uri)[1]
+          @response_content = []
+          @redis.hgetall("events:#{client_name}") do |events|
+            events.each do |check_name, event_json|
+              @response_content << Sensu::JSON.load(event_json)
+            end
+            respond
+          end
+        end
+
+        def get_event
+          uri_match = EVENT_URI.match(@http_request_uri)
+          client_name = uri_match[1]
+          check_name = uri_match[2]
+          @redis.hgetall("events:#{client_name}") do |events|
+            event_json = events[check_name]
+            unless event_json.nil?
+              @response_content = Sensu::JSON.load(event_json)
+              respond
+            else
+              not_found!
+            end
+          end
+        end
+
+        def delete_event
+          uri_match = EVENT_URI.match(@http_request_uri)
+          client_name = uri_match[1]
+          check_name = uri_match[2]
+          @redis.hgetall("events:#{client_name}") do |events|
+            if events.include?(check_name)
+              resolve_event(events[check_name])
+              @response_content = {:issued => Time.now.to_i}
+              accepted!
+            else
+              not_found!
+            end
+          end
+        end
+      end
+    end
+  end
+end
