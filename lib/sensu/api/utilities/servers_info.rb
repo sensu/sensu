5ec54e53276dd9cc958ae145ca600933437dc817
Sean Porter <portertech@gmail.com>
2017-02-20 14:06:03 -0800

    [server-info] updated api /info for server info

---
+++ lib/sensu/api/utilities/servers_info.rb
@@ -0,0 +1,39 @@
+module Sensu
+  module API
+    module Utilities
+      module ServersInfo
+        # Retreive the Sensu servers info.
+        #
+        # @yield [Hash] passes servers info to the callback/block.
+        def servers_info
+          info = []
+          if @redis.connected?
+            @redis.smembers("servers") do |servers|
+              unless servers.empty?
+                puts "servers not empty"
+                servers.each_with_index do |server_id, index|
+                  puts "server_id: #{server_id}"
+                  @redis.get("server:#{server_id}") do |server_json|
+                    puts "server_json: #{server_json}"
+                    unless server_json.nil?
+                      info << Sensu::JSON.load(server_json)
+                    else
+                      @redis.srem("servers", server_id)
+                    end
+                    if index == servers.length - 1
+                      yield(info)
+                    end
+                  end
+                end
+              else
+                yield(info)
+              end
+            end
+          else
+            yield(info)
+          end
+        end
+      end
+    end
+  end
+end
