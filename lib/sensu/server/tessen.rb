46eba97f5b19255c27bdd03efce49d977767ebd9
Sean Porter <portertech@gmail.com>
2018-04-06 13:51:00 -0700

    method yardoc
    Signed-off-by: Sean Porter <portertech@gmail.com>

--- lib/sensu/server/tessen.rb
+++ lib/sensu/server/tessen.rb
@@ -6,19 +6,27 @@ module Sensu
     class Tessen
       attr_accessor :settings, :logger, :redis
 
+      # Create a new instance of Tessen. The instance variable
+      # `@timers` is use to track EventMachine timers for
+      # stopping/shutdown.
       def initialize
         @timers = []
       end
 
+      # Determine if Tessen is enabled (opt-out).
+      #
+      # @return [TrueClass, FalseClass]
       def enabled?
         tessen = @settings.to_hash.fetch(:tessen, {})
         tessen[:enabled] != false
       end
 
+      # Run Tessen, scheduling data reports (every 6h).
       def run
-        schedule_reports
+        schedule_data_reports
       end
 
+      # Stop Tessen, cancelling and clearing timers.
       def stop
         @timers.each do |timer|
           timer.cancel
@@ -28,19 +36,25 @@ module Sensu
 
       private
 
-      def schedule_reports
+      # Schedule data reports, sending data to the Tessen service
+      # immediately and then every 6 hours after that.
+      def schedule_data_reports
         send_data
         @timers << EM::PeriodicTimer.new(21600) do
           send_data
         end
       end
 
+      # Send data to the Tessen service.
       def send_data
         create_data do |data|
           tessen_api_request(data)
         end
       end
 
+      # Create data to be sent to the Tessen service.
+      #
+      # @return [Hash]
       def create_data
         get_install_id do |install_id|
           get_client_count do |client_count|
@@ -75,6 +89,9 @@ module Sensu
         end
       end
 
+      # Get the Sensu installation ID. The ID is randomly generated
+      # and stored in Redis. This ID provides context and allows
+      # multiple Sensu servers to report data for the same installation.
       def get_install_id
         @redis.setnx("tessen:install_id", rand(36**12).to_s(36)) do |created|
           @redis.get("tessen:install_id") do |install_id|
@@ -83,18 +100,26 @@ module Sensu
         end
       end
 
+      # Get the Sensu client count for the installation. This count
+      # currently includes proxy clients.
+      #
+      # @return [Integer]
       def get_client_count
         @redis.scard("clients") do |count|
           yield count.to_i
         end
       end
 
+      # Get the Sensu server count for the installation.
+      #
+      # @return [Integer]
       def get_server_count
         @redis.scard("servers") do |count|
           yield count.to_i
         end
       end
 
+      # Get the Sensu version info for the local Sensu service.
       def get_version_info
         if defined?(Sensu::Enterprise::VERSION)
           ["enterprise", Sensu::Enterprise::VERSION]
@@ -103,6 +128,9 @@ module Sensu
         end
       end
 
+      # Make a Tessen service API request.
+      #
+      # @param data [Hash]
       def tessen_api_request(data)
         note = "this anonymized data helps inform the sensu team about installations"
         note << " - you can choose to opt-out via configuration: {\"tessen\": {\"enabled\": false}}"
