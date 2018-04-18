13f5fd823b83b1466b676e528f26db74ce8297e5
Sean Porter <portertech@gmail.com>
2018-04-18 10:30:39 -0700

    use sensu-spawn 2.4.1, it locks ffi to 1.9.21
    Signed-off-by: Sean Porter <portertech@gmail.com>

--- lib/sensu/daemon.rb
+++ lib/sensu/daemon.rb
@@ -8,7 +8,7 @@ gem "sensu-settings", "10.13.1"
 gem "sensu-extension", "1.5.2"
 gem "sensu-extensions", "1.9.1"
 gem "sensu-transport", "7.1.0"
-gem "sensu-spawn", "2.2.2"
+gem "sensu-spawn", "2.4.1"
 gem "sensu-redis", "2.3.0"
 
 require "time"
