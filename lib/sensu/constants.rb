3d3ca2bb7c6026eac08ba6ea0628aa60d4329b8c
Sean Porter <portertech@gmail.com>
2018-09-04 13:44:55 -0700

    minor version bump, 1.5.0
    Signed-off-by: Sean Porter <portertech@gmail.com>

--- lib/sensu/constants.rb
+++ lib/sensu/constants.rb
@@ -1,7 +1,7 @@
 module Sensu
   unless defined?(Sensu::VERSION)
     # Sensu release version.
-    VERSION = "1.4.3".freeze
+    VERSION = "1.5.0".freeze
 
     # Sensu release information.
     RELEASE_INFO = {
