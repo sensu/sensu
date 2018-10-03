95235297e97c33d6886300e289579278ee3f3b6f
Justin Kolberg <amd.prophet@gmail.com>
2018-10-03 10:58:22 -0700

    add check deletion support to api
    Signed-off-by: Justin Kolberg <amd.prophet@gmail.com>

--- lib/sensu/api/routes/checks.rb
+++ lib/sensu/api/routes/checks.rb
@@ -22,6 +22,26 @@ module Sensu
             not_found!
           end
         end
+
+        # DELETE /checks/:check_name
+        def delete_check
+          # history:*:check_name
+          # history:*:check_name:last_ok
+          # result:*:check_name
+          check_name = parse_uri(CHECK_URI).first
+          @redis.keys("result:*:#{check_name}") do |result_keys|
+            @redis.keys("history:*:#{check_name}") do |history_keys|
+              @redis.keys("history:*:#{check_name}:last_ok") do |last_ok_keys|
+                keys = result_keys.concat(history_keys).concat(last_ok_keys)
+                keys.each do |key|
+                  @redis.del(key)
+                end
+              end
+            end
+          end
+          @response_content = {:issued => Time.now.to_i}
+          accepted!
+        end
       end
     end
   end
