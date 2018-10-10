2db3cd9191e4cbb98fdd09d323124524ea045bc0
Justin Kolberg <amd.prophet@gmail.com>
2018-10-10 11:07:57 -0700

    use smembers instead of keys in check deletion
    Signed-off-by: Justin Kolberg <amd.prophet@gmail.com>

--- lib/sensu/api/routes/checks.rb
+++ lib/sensu/api/routes/checks.rb
@@ -25,18 +25,14 @@ module Sensu
 
         # DELETE /checks/:check_name
         def delete_check
-          # history:*:check_name
-          # history:*:check_name:last_ok
-          # result:*:check_name
           check_name = parse_uri(CHECK_URI).first
-          @redis.keys("result:*:#{check_name}") do |result_keys|
-            @redis.keys("history:*:#{check_name}") do |history_keys|
-              @redis.keys("history:*:#{check_name}:last_ok") do |last_ok_keys|
-                keys = result_keys.concat(history_keys).concat(last_ok_keys)
-                keys.each do |key|
-                  @redis.del(key)
-                end
-              end
+          @redis.smembers("clients") do |clients|
+            result_keys = clients.map {|client_name| "result:#{client_name}:#{check_name}"}
+            history_keys = clients.map {|client_name| "history:#{client_name}:#{check_name}"}
+            last_ok_keys = clients.map {|client_name| "history:#{client_name}:#{check_name}:last_ok"}
+            keys = result_keys.concat(history_keys).concat(last_ok_keys)
+            keys.each do |key|
+              @redis.del(key)
             end
           end
           @response_content = {:issued => Time.now.to_i}
